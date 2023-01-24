af26b9dd6f999f8516acccebbdf25ad20faec71f
DanielPorter000866901 <Daniel.Porter@edu.sait.ca>
2023-01-24 11:35:08 -0700

    added TODOs

--- browsebox-backend/controllers/user-controllers.js
+++ browsebox-backend/controllers/user-controllers.js
@@ -11,11 +11,19 @@ exports.makeUser = (req, res, next) => {
     let password = req.body.password;
     let img = req.body.img;
 
+    // TODO: add verification data is good.
+
     // database makes all users active and not users by default. No change here.
     db.execute(
         'INSERT INTO users (user_name, user_email, user_password, user_img) VALUES (?, ?, ?, ?)',
         [username, email, password, img]
-      );
+      ).then(results => (
+        // if user is added, redirect a POST request (307) to log-in with same username and password given to log-in the user
+        // TODO: upate URL when login abilities are added.
+        res.redirect(307, '/login')
+      )).catch(err => {
+        // TODO: handle error with login.
+      });
 }
 
 /**
@@ -32,6 +40,7 @@ exports.deleteUser = (req, res, next) => {
         [deleteId]
     );
 
-    // TODO: redirect to home page, logged out.
+    // redirect to home page, logged out.
+    res.redirect('/');
 
 }
