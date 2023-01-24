ea0101a7738b8c81f7dc492e97528295ef8f38a4
DanielPorter000866901 <Daniel.Porter@edu.sait.ca>
2023-01-24 11:50:36 -0700

    added console logs for testing

--- browsebox-backend/controllers/user-controllers.js
+++ browsebox-backend/controllers/user-controllers.js
@@ -12,18 +12,34 @@ exports.makeUser = (req, res, next) => {
     let img = req.body.img;
 
     // TODO: add verification data is good.
+    if (
+      username == null || username.trim() == '' || 
+      email == null || email.trim() == '' ||
+      password == null || password.trim() == '' ||
+      img == null || img.trim() == ''
+      ) {
+        console.log("Bad data");
+      } else {
+        console.log(username + " " + email + " " + password + " " + img);
+        
+      }
 
-    // database makes all users active and not users by default. No change here.
+      /*
+
+    // database makes all users active and not admin by default. No change here.
     db.execute(
         'INSERT INTO users (user_name, user_email, user_password, user_img) VALUES (?, ?, ?, ?)',
         [username, email, password, img]
       ).then(results => (
         // if user is added, redirect a POST request (307) to log-in with same username and password given to log-in the user
+
         // TODO: upate URL when login abilities are added.
         res.redirect(307, '/login')
       )).catch(err => {
         // TODO: handle error with login.
       });
+
+      */
 }
 
 /**
