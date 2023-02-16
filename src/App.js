18f2f671e95290f5c9919d258c9058b37a1ec80d
Abner <kold.oktober@gmail.com>
2023-02-16 14:58:23 -0700

    This only sets up the react router in the app.js file and imports the npm for router.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import CreateUserComponent from './Components/UserComponents/CreateUser.component'
+// Import from react-router-dom
+import { BrowserRouter as Router, Routes,
+    Route, Link } from "react-router-dom";
+
+// Import other React Component
 import Header from './pages/Header/Header'
 
 function App() {
   return (
+    <Router>
     <div className="App">
       {/* <CreateUserComponent /> */}
-      <Header />
+      {/*<Header />*/}
     </div>
+        <Routes>
+            <Route exact path="/" element={<Header />} />
+        </Routes>
+
+    </Router>
   )
 }
 
