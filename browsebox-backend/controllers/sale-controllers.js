d9e48c2f8d59e95c0e2f0426b066beaa7822de6d
Henryshi101 <henry.shi@edu.sait.ca>
2023-03-03 13:34:49 -0700

    add search item, delete item, update in in the back end

--- browsebox-backend/controllers/sale-controllers.js
+++ browsebox-backend/controllers/sale-controllers.js
@@ -2,79 +2,105 @@ const db = require("../util/datapool");
 const check = require('../util/checkInput');
 
 /**
- * Make a user in the database. Take data from front end form.
- * Redirect to login in current user
+ * Make a sales item in the database. Takes data from front-end form.
  */
 exports.makeSale = (req, res, next) => {
+  let saleName = req.body.saleName;
+  let description = req.body.description;
+  let price = req.body.price;
+  let img = req.body.img;
+  let id = req.body.id;
 
-    let saleName = req.body.saleName;
-    let description = req.body.description;
-    let price = req.body.price;
-    let img = req.body.img;
-    let id = req.body.id;
-
-    if (
-        !check.checkUsername(saleName) ||
-        !check.checkUsername(description) ||
-        !check.checkPrice(price) ||
-        img === null || img.trim() === ""
-      ) {
-        res.status(500).send('Bad data.')
-      } else {
-        
-        // insert sale item
-        db.execute(
-            'INSERT INTO sales (sale_name, sale_description, sale_price, sale_image, owner) VALUES (?, ?, ?, ?, ?)',
-            [saleName, description, price, img, id]
-          ).then(results => (
-    
-            res.status(200).send("Sales " + saleName + " has been added to the database")
-
-          )).catch(err => {
-            res.status(500).send(err)
-          });
-
-        
-
-      }
-    }
-
-
-
-/**
- * get all item from database
- */
-exports.getItems = function(req, res, next) {
-  db.query("SELECT * FROM sales").then(([rows, fields]) => {
-    // console.log(rows);
-    res.status(200).send(rows);
-  });
-
-  // Can removw after testing function as is.
-  /*
-  db.query(retrieveDataQuery, function(err, results) {
-    if (err) {
-      res.status(500).send({ error: 'Error retrieving items' });
-      return;
-    }
-
-    res.status(200).send({ sales: results });
-
-    // console.log({ sales: results });
-  });
-  */
-
+  if (
+    !check.checkUsername(saleName) ||
+    !check.checkUsername(description) ||
+    !check.checkPrice(price) ||
+    img === null || img.trim() === ""
+  ) {
+    res.status(500).send('Bad data.')
+  } else {
+    // Insert sales item
+    db.execute(
+      'INSERT INTO sales (sale_name, sale_description, sale_price, sale_image, owner) VALUES (?, ?, ?, ?, ?)',
+      [saleName, description, price, img, id]
+    )
+    .then(results => (
+      res.status(200).send("Sales " + saleName + " has been added to the database")
+    ))
+    .catch(err => {
+      res.status(500).send(err)
+    });
+  }
 };
 
+/**
+ * Search for sales items in the database by keyword
+ */
+exports.searchSale = (req, res, next) => {
+  let keyword = req.query.keyword;
 
+  // Search for sales items with the keyword in their name or description
+  db.execute(
+    'SELECT * FROM sales WHERE sale_name LIKE ? OR sale_description LIKE ?',
+    [`%${keyword}%`, `%${keyword}%`]
+  )
+  .then(([rows, fields]) => {
+    res.status(200).send(rows);
+  })
+  .catch(err => {
+    res.status(500).send(err);
+  });
+};
 
+/**
+ * Update a sales item in the database. Takes data from front-end form.
+ */
+exports.updateSale = (req, res, next) => {
+  let saleId = req.params.id;
+  let saleName = req.body.saleName;
+  let description = req.body.description;
+  let price = req.body.price;
+  let img = req.body.img;
+  let id = req.body.id;
 
-// test insert into database
+  if (
+    !check.checkUsername(saleName) ||
+    !check.checkUsername(description) ||
+    !check.checkPrice(price) ||
+    img === null || img.trim() === ""
+  ) {
+    res.status(500).send('Bad data.')
+  } else {
+    // Update sales item
+    db.execute(
+      'UPDATE sales SET sale_name = ?, sale_description = ?, sale_price = ?, sale_image = ?, owner = ? WHERE sale_id = ?',
+      [saleName, description, price, img, id, saleId]
+    )
+    .then(results => (
+      res.status(200).send("Sales " + saleName + " has been updated in the database")
+    ))
+    .catch(err => {
+      res.status(500).send(err)
+    });
+  }
+};
 
-// db.execute(
-//   'INSERT INTO sales (sale_name, owner, sale_description, sale_price, sale_image) VALUES (?, ?, ?, ?, ?)',
-//   ["saleName",3, "description", 12, "img"]
-// );
+/**
+ * Delete a sales item from the database
+ */
+exports.deleteSale = (req, res, next) => {
+  let saleId = req.params.id;
 
-      
+  // Delete sales item
+  db.execute(
+    'DELETE FROM sales WHERE sale_id = ?',
+    [saleId]
+  )
+  .then(results => (
+    res.status(200).send("Sales item " + saleId + " has been deleted from the database")
+  ))
+  .catch(err => {
+    res.status(500).send(err)
+  });
+};
 
