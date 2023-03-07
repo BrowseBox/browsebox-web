d45039e025ee7db37bf5ad1e06db9f932bb9a0f2
Abner <kold.oktober@gmail.com>
2023-03-07 13:09:58 -0700

    Added a table to view user info. Added tab to temp create user.

---
+++ src/Components/AdminDashboard/components/UserList.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "react-bootstrap";
+
+import UserTableRow from "./UserTableRow";
+
+
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:4000/users/")
+            .then(({ data }) => {
+                setUsers(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const DataTable = () => {
+        return users.map((res, i) => {
+            return <UserTableRow obj={res} key={i} />;
+        });
+    };
+
+    return (
+        <div className="table-wrapper">
+            <Table  variant="dark" hover responsive="sm">
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    {/*<th>Password</th>*/}
+                    <th>Rating</th>
+                    {/*<th>Active</th>*/}
+                    {/*<th>Edit or Delete User</th>*/}
+
+                </tr>
+                </thead>
+                <tbody>{DataTable()}</tbody>
+            </Table>
+        </div>
+    );
+};
+
+export default UserList;
