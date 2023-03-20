7a1ca42105a31a967f835a1910951750631fdc9b
Abner <kold.oktober@gmail.com>
2023-03-19 22:23:15 -0600

    This is the create Item form. The dropdown for item-type has a bug. The drag and drop photo has a bug.

--- src/App.js
+++ src/App.js
@@ -6,45 +6,49 @@ import UserList from './Components/AdminDashboard/components/UserList'
 import SignupForm from './pages/Signup/SignupForm'
 import Home from './pages/Home/Home'
 
+
+
 import React from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './Components/AdminDashboard/AdminDashboard'
+import CreateAd2 from "./Components/ItemComponents/CreateAd2";
 
 function App() {
-  const [userLogin, setUserLogin] = React.useState(false)
-  const [user, setUser] = React.useState(null)
+    const [userLogin, setUserLogin] = React.useState(false)
+    const [user, setUser] = React.useState(null)
 
-  useEffect(() => {
-    if (localStorage.getItem('id') !== null) {
-      axios.post('http://localhost:3001/get-user', { id: localStorage.getItem('id') }).then((res) => {
-        setUser(res.data)
-        // console.log(user) // this is null fix it @Jireh
-        // console.log(res.data)
-      })
-    }
-  }, [])
+    useEffect(() => {
+        if (localStorage.getItem('id') !== null) {
+            axios.post('http://localhost:3001/get-user', { id: localStorage.getItem('id') }).then((res) => {
+                setUser(res.data)
+                // console.log(user) // this is null fix it @Jireh
+                // console.log(res.data)
+            })
+        }
+    }, [])
 
-  return (
-    <Router>
-      <Header setUser={setUser} />
-      {/* <SimpleNav /> */}
+    return (
+        <Router>
+            <Header setUser={setUser} />
+            <SimpleNav />
 
-      <Routes>
-        {/* <Route path="/" element={<TestPage1 />} /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/testpage2" element={<TestPage2 />} />
-        <Route path="/user-list" element={<UserList />} />
-        {/* <Route path='/signup' element={<SignupForm />} /> */}
-      </Routes>
-      {/* <CreateUserComponent /> */}
-      {/*<Header setUser={setUser} />*/}
-      {/*{user !== null && <Edit user={user} setUser={setUser} />}*/}
-      {/* <Home user={user} /> */}
-    </Router>
-  )
+            <Routes>
+                {/* <Route path="/" element={<TestPage1 />} /> */}
+                <Route path="/" element={<Home />} />
+                <Route path="/testpage2" element={<TestPage2 />} />
+                <Route path="/user-list" element={<UserList />} />
+                <Route path="/create-ad" element={<CreateAd2 />} />
+                {/* <Route path='/signup' element={<SignupForm />} /> */}
+            </Routes>
+            {/* <CreateUserComponent /> */}
+            {/*<Header setUser={setUser} />*/}
+            {/*{user !== null && <Edit user={user} setUser={setUser} />}*/}
+            {/* <Home user={user} /> */}
+        </Router>
+    )
 }
 
 export default App
--- src/Components/Testing Compnents/SimpleNav.js
+++ src/Components/Testing Compnents/SimpleNav.js
@@ -7,7 +7,7 @@ const SimpleNav = () => {
             <Link to="/testpage2"><button>push me</button></Link>
             <Link to="/user-list"><button>User List</button></Link>
             <Link to="/signup"><button>Create User</button></Link>
-
+            <Link to="/create-ad"><button>Create Ad</button></Link>
         </div>
     )
 }
