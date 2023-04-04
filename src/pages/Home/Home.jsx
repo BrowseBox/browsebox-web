cd28ab7fc4f82d302adad027395534f68cfcc670
EasyKiel <97480726+EasyKiel10@users.noreply.github.com>
2023-04-04 12:07:04 -0600

    Buttons Created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
-import { Box, Stack } from '@mui/material'
+import { Box, Stack, IconButton} from '@mui/material'
 import Item from '../Item/Item'
+
+import BookIcon from '@mui/icons-material/AutoStoriesOutlined';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import FavoritesIcon from '@mui/icons-material/StarOutlineOutlined';
+import SuppliesIcon from '@mui/icons-material/CreateOutlined';
+import BookmarkIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import ComputerIcon from '@mui/icons-material/Computer';
+import VerifiedUserIcon from '@mui/icons-material/HowToReg';
 
 export default function Home() {
   // Test State
@@ -19,6 +27,32 @@ export default function Home() {
 
   return (
     <>
+
+    <Stack direction="row" spacing={14} justifyContent="center" borderRadius={2} padding="5">
+      <IconButton aria-label="favorites">
+        <FavoritesIcon />
+      </IconButton>
+      <IconButton aria-label="saved">
+        <BookmarkIcon />
+      </IconButton>
+      <IconButton aria-label="supplies">
+        <SuppliesIcon />
+      </IconButton>
+      <IconButton aria-label="electronics">
+        <SmartphoneIcon />
+      </IconButton>
+      <IconButton aria-label="books">
+        <BookIcon />
+      </IconButton>
+      <IconButton aria-label="computers/laptop">
+        <ComputerIcon />
+      </IconButton>
+      <IconButton aria-label="verified user">
+        <VerifiedUserIcon />
+      </IconButton>
+    </Stack>
+
+
       <Box>
         <Stack direction="row" spacing={3} m={5}>
           {itemElements}
