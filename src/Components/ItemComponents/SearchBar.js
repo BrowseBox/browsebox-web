4b426812f8f00634aadbb972e7269a5a70a30686
ArisAmidao <amidao.aris03@gmail.com>
2023-04-09 20:29:39 -0600

    Made more progress with search functions

--- src/Components/ItemComponents/SearchBar.js
+++ src/Components/ItemComponents/SearchBar.js
@@ -1,38 +1,48 @@
 import React from "react";
 import { useState, useEffect } from 'react'
 import { InputAdornment, TextField} from '@mui/material'
+import { useNavigate } from "react-router-dom";
 import { FaSearch } from 'react-icons/fa'
-
-
+import HandleSearch from "./HandleSearch";
 
 
 const SearchBar = () => {
-    // Search Functions
+
   const [input, setInput] = useState("")
+  const navigate = useNavigate();
 
   const handleSearch = (event) =>{
     setInput(event.target.value) 
+    
   }
+
+  const handleClick = () => {
+    navigate("/searched")
+  }
+  
 
    return (
     <header>
-        <TextField
+      <TextField
           variant="outlined"
           size="small"
           placeholder='Search...'
-          
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
                 <FaSearch />
               </InputAdornment>
-              
             ),
           }}
           onChange={handleSearch}
         />
+        <button onClick={handleClick}>Find</button>
+        <HandleSearch input={input}/>
     </header>
+    
    )
 }
+
+
 
 export default SearchBar;
