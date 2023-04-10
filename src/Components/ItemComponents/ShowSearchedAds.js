37272cab41b08ff64a265e9db91fcd9c13b5c9fa
ArisAmidao <amidao.aris03@gmail.com>
2023-04-10 13:48:02 -0600

    Working on displaying searched ads

--- src/Components/ItemComponents/ShowSearchedAds.js
+++ src/Components/ItemComponents/ShowSearchedAds.js
@@ -1,9 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import {Typography} from "@mui/material";
 import Ad from './AdList';
 import axios from 'axios';
-import {Typography} from "@mui/material";
 
-const ShowSearchedAds = (input) => {
+
+const ShowSearchedAds = (props) => {
+
+    const keyword = props.prompt;
+    // console.log(keyword)
+
+    const [ads, setAds] = useState([]);
+
+    useEffect(() => {
+        axios
+            .post('http://localhost:3001/search-sale', { keyword })
+            .then((res) => {
+                setAds(res.data);
+                console.log(keyword)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+
+
+    return (
+        <div>
+            <Typography variant="h4" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>Here are your results...</Typography>
+            {ads.length > 0 ? (
+                ads.map((ad) => (
+                    // <Ad key={ad.id} ad={ad} />
+                    <Ad key={ad.sale_id} ad={ad} />
+                ))
+            ) : (
+                <Typography variant="h5" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>We could not find a match....</Typography>
+            )}
+        </div>
+    );
     
 }
 
